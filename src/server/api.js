c0a1fb5853b555effe41ba7d44a36d8f309347be
Martin Laine <martin.laine@simplybusiness.co.uk>
2019-12-30 10:34:48 +0000

    Only return reachable services

--- src/server/api.js
+++ src/server/api.js
@@ -1,5 +1,5 @@
 const fetch = require('node-fetch');
-const { parse } = require('date-fns');
+const { parse, differenceInMinutes } = require('date-fns');
 const Rail = require('national-rail-darwin');
 
 const railAPI = new Rail();
@@ -12,24 +12,31 @@ function timeFromRailDeparture({ std, etd }) {
   return parse(etd === 'On time' ? std : etd, 'HH:mm', new Date());
 }
 
-function departureSort(a, b) {
+function departureSortFunction(a, b) {
   if (a.time < b.time) return -1;
   if (a.time > b.time) return 1;
   return 0;
 }
 
-function fetchTrainStationDepartures(station) {
+function concatAndSort(departures) {
+  return [].concat(...departures).sort(departureSortFunction);
+}
+
+function fetchTrainStationDepartures({ station, minutesToStation }) {
+  const now = new Date();
   return new Promise((resolve, reject) => {
     railAPI.getDepartureBoard(station, {}, (err, { trainServices }) => {
       if (err) return reject(err);
 
       resolve(
-        trainServices.map((r) => ({
-          id: r['serviceId'],
-          line: r['operator'].replace(/^London /, ''),
-          destination: r['destination']['name'],
-          time: timeFromRailDeparture(r),
-        })),
+        trainServices
+          .map((r) => ({
+            id: r['serviceId'],
+            line: r['operator'].replace(/^London /, ''),
+            destination: r['destination']['name'],
+            time: timeFromRailDeparture(r),
+          }))
+          .filter((d) => differenceInMinutes(d.time, now) >= minutesToStation),
       );
     });
   });
@@ -37,25 +44,26 @@ function fetchTrainStationDepartures(station) {
 
 async function fetchTrainDepartures(stations) {
   const departures = await Promise.all(stations.map(fetchTrainStationDepartures));
-  return [].concat(...departures).sort(departureSort);
+  return concatAndSort(departures);
 }
 
-async function fetchBusStopDepartures(stop) {
+async function fetchBusStopDepartures({ stop, minutesToStop }) {
   const response = await fetch(`${TFL_API_URL}/StopPoint/${stop}/Arrivals?app_id=${TFL_APP_ID}&app_key=${TFL_APP_KEY}`);
   const departures = await response.json();
-  return departures.map((d) => {
-    return {
+  const now = new Date();
+  return departures
+    .map((d) => ({
       id: d['id'],
       line: d['lineName'],
-      destination: d['towards'],
+      destination: d['destinationName'],
       time: new Date(d['expectedArrival']),
-    };
-  });
+    }))
+    .filter((d) => differenceInMinutes(d.time, now) >= minutesToStop);
 }
 
 async function fetchBusDepartures(stops) {
   const departures = await Promise.all(stops.map(fetchBusStopDepartures));
-  return [].concat(...departures).sort(departureSort);
+  return concatAndSort(departures);
 }
 
 module.exports = { fetchBusDepartures, fetchTrainDepartures };
