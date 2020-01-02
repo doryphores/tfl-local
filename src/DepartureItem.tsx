9635feb1b0621755eb98198885aec7181f4580dc
Martin Laine <martin.laine@simplybusiness.co.uk>
2020-01-02 08:21:05 +0000

    Add optimistic UI time updates

--- src/DepartureItem.tsx
+++ src/DepartureItem.tsx
@@ -1,7 +1,8 @@
 /** @jsx jsx */
-import { jsx, SxProps } from 'theme-ui';
+import { jsx } from 'theme-ui';
 import { Badge } from '@theme-ui/components';
 import { formatDistance, format } from 'date-fns';
+import { useEffect, useState } from 'react';
 
 export interface Props {
   mode: 'bus' | 'train';
@@ -10,48 +11,52 @@ export interface Props {
   time: Date;
 }
 
-const textOverflow: SxProps['sx'] = {
-  overflow: 'hidden',
-  textOverflow: 'ellipsis',
-  whiteSpace: 'nowrap',
-};
-
-const DepartureItem: React.FC<Props> = ({ mode, line, direction, time }) => (
-  <li
-    sx={{
-      display: 'flex',
-      '& + &': {
-        pt: 2,
-        mt: 2,
-        borderTopColor: 'muted',
-        borderTopStyle: 'solid',
-        borderTopWidth: '1px',
-      },
-    }}
-  >
-    <Badge
-      sx={{ whiteSpace: 'nowrap' }}
-      variant={mode === 'bus' ? 'bus' : line.toLowerCase()}
-    >
-      {line}
-    </Badge>
-    <span
+const DepartureItem: React.FC<Props> = ({ mode, line, direction, time }) => {
+  const [now, setNow] = useState(new Date());
+  useEffect(() => {
+    let timer = setInterval(() => setNow(new Date()), 1000);
+    return () => clearInterval(timer);
+  }, []);
+  return (
+    <li
       sx={{
-        ...textOverflow,
-        flex: '1',
-        py: 1,
-        px: 2,
+        display: 'flex',
+        // alignItems: 'center',
+        '& + &': {
+          pt: [1, 2, 3],
+          mt: [1, 2, 3],
+          borderTopColor: 'muted',
+          borderTopStyle: 'solid',
+          borderTopWidth: '1px',
+        },
       }}
     >
-      {direction}
-    </span>
-    <span
-      title={format(time, 'HH:mm')}
-      sx={{ py: 1, whiteSpace: 'nowrap', fontWeight: 'time' }}
-    >
-      {formatDistance(time, new Date())}
-    </span>
-  </li>
-);
+      <Badge
+        sx={{ whiteSpace: 'nowrap' }}
+        variant={mode === 'bus' ? 'bus' : line.toLowerCase()}
+      >
+        {line}
+      </Badge>
+      <span
+        sx={{
+          flex: '1',
+          px: 2,
+          py: 1,
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+          whiteSpace: 'nowrap',
+        }}
+      >
+        {direction}
+      </span>
+      <span
+        title={format(time, 'HH:mm')}
+        sx={{ whiteSpace: 'nowrap', fontWeight: 'time', py: 1 }}
+      >
+        {formatDistance(time, now, { addSuffix: true })}
+      </span>
+    </li>
+  );
+};
 
 export default DepartureItem;
