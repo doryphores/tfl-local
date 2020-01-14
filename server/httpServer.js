2b7cc952151f28a5ca6673c2633bbf5bf98be6bb
Martin Laine <martin.laine@gmail.com>
2020-01-14 21:54:31 +0000

    Correct build path for server

--- server/httpServer.js
+++ server/httpServer.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const assetPath = path.join(__dirname, '../../build');
+const assetPath = path.join(__dirname, '../build');
 
 app.use(express.static(assetPath));
 
