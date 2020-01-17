0ef85cef56ec5055b55de82ea310b9c40104112a
Martin Laine <martin.laine@simplybusiness.co.uk>
2020-01-17 22:07:29 +0000

    WS reconnection mechanism

--- src/socket.ts
+++ src/socket.ts
@@ -2,15 +2,20 @@ const SOCKET_URL = `ws://${window.location.hostname}:${process.env['REACT_APP_SE
 const RECONNECT_DELAY = 5000;
 
 let webSocket: WebSocket;
-let timer: NodeJS.Timeout;
+let reconnectionTimeout: NodeJS.Timeout;
 
 export function connect(callback: (msg: any) => void): void {
-  if (timer) clearTimeout(timer);
+  if (reconnectionTimeout) clearTimeout(reconnectionTimeout);
   console.debug('Connecting to web socket');
+
+  let connectionTimeout: NodeJS.Timeout = setTimeout(() => {
+    if (webSocket.readyState === 0) webSocket.close();
+  }, RECONNECT_DELAY);
 
   webSocket = new WebSocket(SOCKET_URL);
 
   webSocket.addEventListener('open', () => {
+    clearTimeout(connectionTimeout);
     console.debug('Web socket connected');
   });
 
@@ -22,6 +27,6 @@ export function connect(callback: (msg: any) => void): void {
   webSocket.addEventListener('close', (event: CloseEvent): void => {
     console.debug('Web socket disconnected');
     if (webSocket.readyState < 2) webSocket.close();
-    timer = setTimeout(() => connect(callback), RECONNECT_DELAY);
+    reconnectionTimeout = setTimeout(() => connect(callback), RECONNECT_DELAY);
   });
 }
