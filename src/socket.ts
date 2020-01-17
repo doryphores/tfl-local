3619f668f076c3822624fbbc1b5d70b4cb889595
Martin Laine <martin.laine@simplybusiness.co.uk>
2020-01-17 19:20:48 +0000

    Improve UI logging

--- src/socket.ts
+++ src/socket.ts
@@ -6,21 +6,21 @@ let timer: NodeJS.Timeout;
 
 export function connect(callback: (msg: any) => void): void {
   if (timer) clearTimeout(timer);
-  console.debug('Connecting');
+  console.debug('Connecting to web socket');
 
   webSocket = new WebSocket(SOCKET_URL);
 
   webSocket.addEventListener('open', () => {
-    console.debug('Connected');
+    console.debug('Web socket connected');
   });
 
   webSocket.addEventListener('message', (event: MessageEvent): void => {
-    const data = JSON.parse(event.data);
-    callback(data);
+    console.debug('Message received');
+    callback(JSON.parse(event.data));
   });
 
   webSocket.addEventListener('close', (event: CloseEvent): void => {
-    console.debug('Disconnected');
+    console.debug('Web socket disconnected');
     if (webSocket.readyState < 2) webSocket.close();
     timer = setTimeout(() => connect(callback), RECONNECT_DELAY);
   });
