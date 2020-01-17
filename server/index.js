404e2246d706ca4d6c4c51625a6866dfe68be47d
Martin Laine <martin.laine@simplybusiness.co.uk>
2020-01-17 19:22:54 +0000

    Log server starts with build number

--- server/index.js
+++ server/index.js
@@ -6,11 +6,14 @@ const { fetchTrainDepartures, fetchBusDepartures } = require('./api');
 const STOPS = require('./stops');
 const logger = require('./logger');
 
+const BUILD_NUMBER = process.env['BUILD_NUMBER'] || 'DEV';
 const CACHE = { bus: [], train: [] };
 const DELAY = 10000;
 
 const server = httpServer.start();
 wsServer.start(server, () => CACHE);
+
+logger.notice(`Started server (build number: ${BUILD_NUMBER})`);
 
 async function updateStop(mode, stops) {
   try {
@@ -28,6 +31,7 @@ async function updateStop(mode, stops) {
 
     logger.debug(`Updated ${mode} departures in ${requestDuration}s`);
     CACHE[mode] = departures;
+    // TODO: only send mode updates (bus or train)
     wsServer.sendToAll(CACHE);
   } catch (err) {
     logger.error(err.message);
